588413b444df479f5bf2935a9a1d367c7ba769bc
Sundance <sundancekid1108@gmail.com>
2019-09-02 20:46:38 +0900

    Like 기능추가, addComment 기능추가
    Like, addComment 완료

---
+++ Instaclone-backend/src/api/Like/toggleLike/toggleLike.js
@@ -0,0 +1,53 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+
+// {
+// 	"Authorization": "Bearer KEY"
+// }
+// playground에서  HTTP HEADER에 값 넣기
+
+export default {
+    Mutation: {
+        toggleLike: async(_, args, { request }) => {
+            isAuthenticated(request);
+            const { postId } = args;
+            const { user } = request;
+            const filterOptions = {
+                AND: [{
+                        user: {
+                            id: user.id
+                        }
+                    },
+                    {
+                        post: {
+                            id: postId
+                        }
+                    }
+                ]
+            };
+            try {
+                const existingLike = await prisma.$exists.like(filterOptions);
+                if (existingLike) {
+                    await prisma.deleteManyLikes(filterOptions);
+                } else {
+                    await prisma.createLike({
+                        user: {
+                            connect: {
+                                id: user.id
+                            }
+                        },
+                        post: {
+                            connect: {
+                                id: postId
+                            }
+                        }
+                    });
+                }
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
+};
